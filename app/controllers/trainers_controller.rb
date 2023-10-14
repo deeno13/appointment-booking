fda71e52da78386dc409edadc430a8a0fc5f56f4
Awang Hafizuddin <hafizxx13@gmail.com>
2023-10-14 21:36:18 +0800

    Add basic CRUD for Trainers controller

--- app/controllers/trainers_controller.rb
+++ app/controllers/trainers_controller.rb
@@ -1,13 +1,49 @@
 class TrainersController < ApplicationController
-  def index
-  end
+  before_action :set_trainer, only: [:show, :edit, :update, :destroy]
 
-  def new
+  def index
+    @trainers = Trainer.all
   end
 
   def show
   end
 
+  def new
+    @trainer = Trainer.new
+  end
+
+  def create
+    @trainer = Trainer.new(trainer_params)
+    if @trainer.save
+      redirect_to @trainer, notice: 'Trainer was successfully created.'
+    else
+      render :new
+    end
+  end
+
   def edit
+  end
+
+  def update
+    if @trainer.update(trainer_params)
+      redirect_to @trainer, notice: 'Trainer was successfully updated.'
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @trainer.destroy
+    redirect_to trainers_url, notice: 'Trainer was successfully destroyed.'
+  end
+
+  private
+
+  def set_trainer
+    @trainer = Trainer.find(params[:id])
+  end
+
+  def trainer_params
+    params.require(:trainer).permit(:name)
   end
 end
