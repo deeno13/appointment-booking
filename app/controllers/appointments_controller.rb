0d385749257807bb4eff2a9ee951f92c01238a36
Awang Hafizuddin <hafizxx13@gmail.com>
2023-10-14 21:37:05 +0800

    Add basic CRUD for Appointments controller

--- app/controllers/appointments_controller.rb
+++ app/controllers/appointments_controller.rb
@@ -1,13 +1,54 @@
 class AppointmentsController < ApplicationController
+  before_action :set_trainer
+  before_action :set_appointment, only: [:show, :edit, :update, :destroy]
+
   def index
+    @appointments = @trainer.appointments
   end
 
   def show
   end
 
   def new
+    @appointment = @trainer.appointments.new
+  end
+
+  def create
+    @appointment = @trainer.appointments.new(appointment_params)
+    if @appointment.save
+      redirect_to trainer_appointments_path(@trainer), notice: 'Appointment was successfully created.'
+    else
+      render :new
+    end
   end
 
   def edit
+  end
+
+  def update
+    if @appointment.update(appointment_params)
+      redirect_to trainer_appointments_path(@trainer), notice: 'Appointment was successfully updated.'
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @appointment.destroy
+    redirect_to trainer_appointments_path(@trainer), notice: 'Appointment was successfully destroyed.'
+  end
+
+  private
+
+  def set_trainer
+    @trainer = Trainer.find(params[:trainer_id])
+  end
+
+  def set_appointment
+    @appointment = Trainer.appointments.find(params[:id])
+  end
+
+  def appointment_params
+    require(:appointment).permit(:start_time, :end_time)
   end
 end
