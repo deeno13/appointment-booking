4f9f76286129c6c51e646e12f27451ca8f3385b9
Awang Hafizuddin <hafizxx13@gmail.com>
2023-10-18 15:51:18 +0800

    Add end_time after start_time validation for Availability

--- app/models/availability.rb
+++ app/models/availability.rb
@@ -5,5 +5,18 @@ class Availability < ApplicationRecord
   validates :start_time, presence: true
   validates :end_time, presence: true
 
+  validate :end_time_after_start_time
+
+
   enum day_of_week: %w[Sunday Monday Tuesday Wednesday Thursday Friday Saturday]
+
+  private
+
+  def end_time_after_start_time
+    return if end_time.blank? || start_time.blank?
+
+    if end_time <= start_time
+      errors.add(:end_time, "must be after the start time")
+    end
+  end
 end
