8ec32d8196e46f728ef47042742e72f07fd1f816
Awang Hafizuddin <hafizxx13@gmail.com>
2023-10-18 16:23:43 +0800

    Move inline errors to application helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+  def inline_errors(model, field)
+    errors_list = ''
+    if model.errors[field].any?
+      model.errors.full_messages_for(field).each do |error|
+        errors_list += "<li class='text-red-500 mt-1'>#{error}</li>"
+      end
+    end
+    return "<ul#{errors_list}</ul>".html_safe
+  end
 end
