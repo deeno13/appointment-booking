808fc57e783e8d08cc7a5af3466f6ce0d87e823a
Awang Hafizuddin <hafizxx13@gmail.com>
2023-10-20 13:38:06 +0800

    Connect flatpickr datetime picker to Appointment datetime fields

--- app/javascript/controllers/flatpickr_controller.js
+++ app/javascript/controllers/flatpickr_controller.js
@@ -5,5 +5,49 @@ import flatpickr from "flatpickr";
 export default class extends Controller {
   connect() {
     console.log("connected");
+
+    flatpickr(".start-time-field", {
+      enableTime: true,
+      dateFormat: "Y-m-d H:i",
+      minDate: this.setDefaultDateTime(),
+      altInput: true,
+      altFormat: "F j, Y h:i K",
+      maxTime: this.setDefaultDateTime(),
+      minuteIncrement: 60,
+      disableMobile: true,
+      defaultDate: this.setDefaultDateTime(),
+    });
+
+    flatpickr(".end-time-field", {
+      enableTime: true,
+      dateFormat: "Y-m-d H:i",
+      minDate: "today",
+      altInput: true,
+      altFormat: "F j, Y h:i K",
+      minuteIncrement: 60,
+      disableMobile: true,
+      defaultDate: this.setEndTime(),
+    });
+  }
+
+  getCurrentDateTime() {
+    return new Date();
+  }
+
+  setDefaultDateTime() {
+    const defaultDateTime = this.getCurrentDateTime();
+
+    defaultDateTime.setHours(defaultDateTime.getHours() + 1);
+    defaultDateTime.setMinutes(0);
+
+    return defaultDateTime;
+  }
+
+  setEndTime() {
+    const startTime = new Date(
+      document.querySelector(".start-time-field").value
+    );
+
+    return startTime.setHours(startTime.getHours() + 1);
   }
 }
