fb4cb84ef5ebecc17cba8739e8622b4455195615
Awang Hafizuddin <hafizxx13@gmail.com>
2023-10-23 00:24:19 +0800

    Fix appointment end time not set on first load

--- app/javascript/controllers/flatpickr_controller.js
+++ app/javascript/controllers/flatpickr_controller.js
@@ -16,6 +16,8 @@ export default class extends Controller {
       disableMobile: true,
       defaultDate: this.setDefaultDateTime(),
     });
+
+    this.setEndTime();
   }
 
   getCurrentDateTime() {
